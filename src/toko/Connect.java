23233cca581c919d88b372a93467ea1704b75568
PrimaPras <50481518+PrimaPras@users.noreply.github.com>
2020-12-15 18:56:57 +0700

    First push

---
+++ src/toko/Connect.java
@@ -0,0 +1,20 @@
+package toko;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connect {
+	private String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private String url = "jdbc:mysql://localhost:3306/bpltb";
+    private String username = "root";
+    private String password = "";
+
+    public Connection getConn() throws SQLException {
+        Connection connection = DriverManager.getConnection(
+                url,username,password
+        );
+
+        return connection;
+    }
+}
